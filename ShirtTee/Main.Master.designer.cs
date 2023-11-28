25f0cad21b757f5fc08ebddb3818a94387872cc2
GohChunYen <chunyengoh030503@gmail.com>
2023-11-29 02:56:19 +0800

    homepage pending final check

--- ShirtTee/Main.Master.designer.cs
+++ ShirtTee/Main.Master.designer.cs
@@ -15,13 +15,13 @@ namespace ShirtTee
     {
 
         /// <summary>
-        /// head control.
+        /// SiteMapDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
@@ -31,5 +31,14 @@ namespace ShirtTee
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// productMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu productMenu;
     }
 }
