db57a60c90a3e266c0d2757bb4a2fbb36efc8c78
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-11-28 21:22:55 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./ShirtTee/*.{aspx,Master}",
-        'node_modules/preline/dist/*.js',    ],
-  theme: {
-      extend: {
-          gridTemplateRows: {
-              '[auto,auto,1fr]': 'auto auto 1fr',
-          },
-      },
-  },
+        'node_modules/preline/dist/*.js',],
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                '[auto,auto,1fr]': 'auto auto 1fr',
+            },
+        },
+    },
     plugins: [
         require('preline/plugin'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-  ],
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
 
