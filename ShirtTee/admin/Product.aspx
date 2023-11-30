2802f62375f4e252dabaab361a1561538aa6e727
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-11-30 23:10:32 +0800

    sitemap on9

---
+++ ShirtTee/admin/Product.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/MainAdmin.Master" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="ShirtTee.admin.Product" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- ShirtTee/admin/Product.aspx.cs
+++ ShirtTee/admin/Product.aspx.cs
@@ -7,12 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace ShirtTee.admin
 {
-    public partial class MainAdmin : System.Web.UI.MasterPage
+    public partial class Product : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //SiteMapDataSource1.SiteMapProvider = "AdminSiteMapProvider";
-        }
 
+        }
     }
 }
