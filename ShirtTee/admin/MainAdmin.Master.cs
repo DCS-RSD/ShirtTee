cb6f601b4338470e7dd2e87ca4c3a15869fe2060
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-01 21:07:11 +0800

    fcking nav

--- ShirtTee/admin/MainAdmin.Master.cs
+++ ShirtTee/admin/MainAdmin.Master.cs
@@ -11,6 +11,7 @@ namespace ShirtTee.admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string activeNavClass = "flex items-center gap-x-3.5 py-2 px-2.5  text-sm bg-gray-100 text-slate-700 rounded-lg dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600";
             // Get the current page name without the path and extension
             string currentPage = System.IO.Path.GetFileNameWithoutExtension(Request.Url.AbsolutePath);
 
@@ -18,13 +19,13 @@ namespace ShirtTee.admin
             switch (currentPage)
             {
                 case "Dashboard":
-                    dashboardLink.Attributes["class"] += " active";
+                    dashboardLink.Attributes["class"] = activeNavClass;
                     break;
                 case "AnotherPage":
-                    customerLink.Attributes["class"] += " active";
+                    customerLink.Attributes["class"] = activeNavClass;
                     break;
                 case "Product":
-                    productLink.Attributes["class"] += " active";
+                    productLink.Attributes["class"] = activeNavClass;
                     break;
                     // Add cases for other pages as needed
             }
