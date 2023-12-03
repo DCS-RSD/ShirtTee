ffa34363aaf0cdeabe27e64d18fe2fca9fc9d6a9
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-03 19:54:46 +0800

    styling table

--- ShirtTee/admin/MainAdmin.Master.cs
+++ ShirtTee/admin/MainAdmin.Master.cs
@@ -11,7 +11,7 @@ namespace ShirtTee.admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string activeNavClass = "flex items-center gap-x-3.5 py-2 px-2.5  text-sm bg-gray-100 text-slate-700 rounded-lg dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600";
+            string activeNavClass = "flex items-center gap-x-3.5 py-2 px-2.5  text-sm bg-gray-100 text-slate-700 rounded-lg dark:bg-gray-900  dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600";
             // Get the current page name without the path and extension
             string currentPage = System.IO.Path.GetFileNameWithoutExtension(Request.Url.AbsolutePath);
 
