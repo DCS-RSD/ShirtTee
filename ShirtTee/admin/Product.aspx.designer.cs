6a7aaf0931e266436773b5befee2d53e10b47e7c
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-04 12:57:41 +0800

    paginator

--- ShirtTee/admin/Product.aspx.designer.cs
+++ ShirtTee/admin/Product.aspx.designer.cs
@@ -33,12 +33,21 @@ namespace ShirtTee.admin
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
 
         /// <summary>
-        /// Repeater1 control.
+        /// ListView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+        protected global::System.Web.UI.WebControls.ListView ListView1;
+
+        /// <summary>
+        /// DataPager2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataPager DataPager2;
     }
 }
