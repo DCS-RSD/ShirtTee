cf7e032457171414fae1d0a03f7b9f2984353f50
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-09 23:27:31 +0800

    upgrade performance

--- ShirtTee/DBconnection.cs
+++ ShirtTee/DBconnection.cs
@@ -9,24 +9,25 @@ namespace ShirtTee
 {
     public class DBconnection
     {
-        private string connectionString = 
+        private string connectionString =
             ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
         private Dictionary<string, string> queryList;
 
-        public DBconnection()
+        private void LazyInitializeQueries()
         {
-            this.queryList = new Dictionary<string, string>();
-            InitializeQueries();
-        }
-
-        private void InitializeQueries()
-        {
-            queryList.Add("GetProductDetails", "SELECT P.*, C.* FROM Product AS P INNER JOIN Category AS C ON P.category_id = C.category_id WHERE Product_ID=@ProductId");
+            if (this.queryList == null)
+            {
+                queryList = new Dictionary<string, string>();
+                queryList.Add("GetProductDetails", "SELECT P.*, C.* FROM Product AS P INNER JOIN Category AS C ON P.category_id = C.category_id WHERE Product_ID=@ProductId");
+            }
 
         }
 
         public SqlCommand ExecuteQuery(string queryKey, SqlParameter[] parameters)
         {
+            //Ensure the dictionary only init for first access (better performance)
+            LazyInitializeQueries();
+
             // Ensure the query key exists in the dictionary
             if (!queryList.ContainsKey(queryKey))
             {
