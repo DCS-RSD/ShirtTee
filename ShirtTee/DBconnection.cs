de0e2677b719db2de0953a998cc061b6462bd6bb
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-09 23:19:18 +0800

    "DBconnection, reusable"

---
+++ ShirtTee/DBconnection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ShirtTee
+{
+    public class DBconnection
+    {
+        private string connectionString = 
+            ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        private Dictionary<string, string> queryList;
+
+        public DBconnection()
+        {
+            this.queryList = new Dictionary<string, string>();
+            InitializeQueries();
+        }
+
+        private void InitializeQueries()
+        {
+            queryList.Add("GetProductDetails", "SELECT P.*, C.* FROM Product AS P INNER JOIN Category AS C ON P.category_id = C.category_id WHERE Product_ID=@ProductId");
+
+        }
+
+        public SqlCommand ExecuteQuery(string queryKey, SqlParameter[] parameters)
+        {
+            // Ensure the query key exists in the dictionary
+            if (!queryList.ContainsKey(queryKey))
+            {
+                return null;
+                //throw new ArgumentException($"Query with key '{queryKey}' not found.");
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(queryList[queryKey], connection);
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            return command;
+
+
+        }
+    }
+}
