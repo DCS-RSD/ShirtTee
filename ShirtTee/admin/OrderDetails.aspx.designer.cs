63acca3e6ee8e27d2d96883e81bf5d1a95ac3f0a
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-12-14 08:57:00 +0800

    pending

--- ShirtTee/admin/OrderDetails.aspx.designer.cs
+++ ShirtTee/admin/OrderDetails.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace ShirtTee.admin
         protected global::System.Web.UI.WebControls.Label lblOrderDate;
 
         /// <summary>
+        /// progressBar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl progressBar;
+
+        /// <summary>
         /// lblCustId control.
         /// </summary>
         /// <remarks>
