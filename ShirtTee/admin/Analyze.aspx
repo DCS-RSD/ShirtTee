a908367790414832224dc17c4d20c2d09adc16a1
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-01-01 20:24:01 +0800

    pending

--- ShirtTee/admin/Analyze.aspx
+++ ShirtTee/admin/Analyze.aspx
@@ -34,7 +34,8 @@
                     </div>
                 </div>
 
-                <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 rounded-b-xl dark:border-gray-700 dark:text-gray-400 dark:hover:bg-slate-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">View reports
+                <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 rounded-b-xl dark:border-gray-700 dark:text-gray-400 dark:hover:bg-slate-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    href="AnalyzeProduct.aspx">Which product is top seller?
                     <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="m9 18 6-6-6-6" />
                     </svg>
@@ -66,7 +67,7 @@
                 </div>
 
                 <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 rounded-b-xl dark:border-gray-700 dark:text-gray-400 dark:hover:bg-slate-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" 
-                    href="AnalyzeCustomer.aspx">View reports
+                    href="AnalyzeCustomer.aspx">Who are the top customers in terms of product purchases?
                     <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="m9 18 6-6-6-6" />
                     </svg>
@@ -94,10 +95,11 @@
                     </div>
                 </div>
 
-                <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 rounded-b-xl dark:border-gray-700 dark:text-gray-400 dark:hover:bg-slate-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">View reports
-        <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="m9 18 6-6-6-6" />
-        </svg>
+                <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 rounded-b-xl dark:border-gray-700 dark:text-gray-400 dark:hover:bg-slate-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" 
+                    href="">How are the product sales performing?
+                    <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="m9 18 6-6-6-6" />
+                    </svg>
                 </a>
             </div>
             <!-- End Card -->
---
+++ ShirtTee/admin/AnalyzeProduct.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShirtTee.admin
+{
+    public partial class AnalyzeProduct : System.Web.UI.Page
+    {
+
+        const string query = @"
+SELECT
+    P.product_ID,
+    P.product_name,
+
+FROM
+    Product P
+JOIN
+    Product_Details PD ON P.product_ID = PD.product_ID
+LEFT JOIN
+    Order_Details OD ON PD.product_details_ID = OD.product_details_ID
+LEFT JOIN
+    [Order] O ON OD.order_ID = O.order_ID";
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            if (IsPostBack)
+            {
+                try
+                {
+                    if (ddlYear.SelectedIndex != 0 && ddlMonth.SelectedIndex != 0)
+                    {
+                        SqlDataSource1.SelectCommand = query + " WHERE Year(order_date) = @year AND Month(order_date) = @month";
+                        SqlDataSource1.SelectParameters.Clear();
+                        SqlDataSource1.SelectParameters.Add("year", ddlYear.SelectedValue);
+                        SqlDataSource1.SelectParameters.Add("month", ddlMonth.SelectedValue);
+
+                    }
+                    else if (ddlYear.SelectedIndex != 0)
+                    {
+                        SqlDataSource1.SelectCommand = query + " WHERE Year(order_date) = @year";
+                        SqlDataSource1.SelectParameters.Clear();
+                        SqlDataSource1.SelectParameters.Add("year", ddlYear.SelectedValue);
+                    }
+                    else if (ddlMonth.SelectedIndex != 0)
+                    {
+                        SqlDataSource1.SelectCommand = query + " WHERE Month(order_date) = @month";
+                        SqlDataSource1.SelectParameters.Clear();
+                        SqlDataSource1.SelectParameters.Add("month", ddlMonth.SelectedValue);
+                    }
+                    else
+                    {
+                        SqlDataSource1.SelectCommand = query;
+                        SqlDataSource1.SelectParameters.Clear();
+                    }
+                    SqlDataSource1.SelectCommand += " GROUP BY P.product_ID, P.product_name";
+                    ListView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message + "\n" + query + "\n" + SqlDataSource1.SelectCommand);
+                }
+                DataPager2.PageSize = int.MaxValue;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnExcel_Click(object sender, EventArgs e)
+        {
+            generateFile(".xls");
+        }
+
+        private void generateFile(string extension)
+        {
+            hideLinkButton();
+
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=customer-report-" + DateTime.Now.ToString("yyyy-MM-dd_HH:mm:ss") + extension);
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.ms-excel";
+            using (StringWriter sw = new StringWriter())
+            {
+                HtmlTextWriter hw = new HtmlTextWriter(sw);
+                ListView1.RenderControl(hw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+        }
+
+        private void hideLinkButton()
+        {
+            string[] linkButtonIds = { "LinkButton1", "LinkButton2", "LinkButton3" };
+
+            foreach (string linkButtonId in linkButtonIds)
+            {
+                LinkButton linkButton = (LinkButton)ListView1.FindControl(linkButtonId);
+
+                if (linkButton != null)
+                {
+                    linkButton.Visible = false;
+                }
+            }
+        }
+
+        protected void btnPrint_Click(object sender, EventArgs e)
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "Print", "printListView();", true);
+        }
+    }
+}
