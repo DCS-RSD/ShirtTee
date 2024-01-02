80dd3ba210f3c501f11b65edaba5d70ba7a9bc9b
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-01-02 15:24:47 +0800

    sales

--- ShirtTee/admin/Analyze.aspx.designer.cs
+++ ShirtTee/admin/Analyze.aspx.designer.cs
@@ -42,6 +42,42 @@ namespace ShirtTee.admin
         protected global::System.Web.UI.WebControls.Label lblTotalSales;
 
         /// <summary>
+        /// lblModalTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblModalTitle;
+
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// lblModalTotalSales control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblModalTotalSales;
+
+        /// <summary>
         /// ddlYear control.
         /// </summary>
         /// <remarks>
